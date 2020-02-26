1eba44f927cc093dd0290cdedb85bfccc9512548
xuguangye7 <732654087@qq.com>
2020-02-26 09:22:46 +0800

    htmlhint

---
+++ rectangle.js
@@ -0,0 +1,19 @@
+
+$(function(){
+    var $width=$('#width'),
+              $height=$('#height'),
+                    $btnCal=$('#calculate'),
+                          $perimeter=$('#perimeter'),
+                                $area=$('#area');
+    $btnCal.click(function(){
+          var w =Number($width.val()),
+              h =Number($height.val());
+        var p = Math.round(2*(w+h)*Math.pow(10,3))/Math.pow(10,3),
+              a = Math.round(w*h*Math.pow(10,3))/Math.pow(10,3);
+        $perimeter.val(p);
+            $area.val(a);
+              
+    });
+
+});
+
