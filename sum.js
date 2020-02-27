f2cbb1332f57896d2c78c87607b0edbd4919b1a4
xuguangye7 <732654087@qq.com>
2020-02-27 16:59:23 +0800

    mocha-be

---
+++ sum.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+if(process.argv.length !=4){
+  console.log('sum x y');
+  process.exit(1);
+}
+
+var x= Number(process.argv[2]),
+    y= Number(process.argv[3]);
+
+const add = require('./calc.js');
+
+console.log(`${x}+${y}=${add(x,y)}`);
+
+
---
+++ test/cale.test.js
@@ -0,0 +1,11 @@
+const add = require('../calc.js'),
+      expect = require('chai').expect;
+
+describe('加法单元测试',function(){
+  it('0+0=0',function(){
+    expect(add(0,0)).to.be.equal(0);
+  });
+  it('1+0=0',function(){
+    expect(add(1,0)).to.be.equal(1);
+  });
+})
